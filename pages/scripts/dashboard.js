549f22378cb7f94a36a0c56b6a47e0351b6b234d
Scott Bader <58278360+ManWithACap@users.noreply.github.com>
2024-05-27 05:13:23 +0000

    nav buttons

--- pages/scripts/dashboard.js
+++ pages/scripts/dashboard.js
@@ -1,21 +1,5 @@
-document.getElementById("sendBtn").addEventListener("click", () => {
-	const webhookUrl = 'https://discord.com/api/webhooks/1243821069534630029/a8iSk6OsE9IuEekg5KxEypfuhURB2O9cFpeQkqaws4HEZsJswD41v-LNnT2o_Wds1-k1';
-	const message = {
-		content: 'Hello from GitHub Pages!'
-	};
+window.onload = function() {
+	document.getElementById("username").innerHTML = window.localStorage.getItem("username");
 
-	fetch(webhookUrl, {
-		method: 'POST',
-		headers: {
-		  'Content-Type': 'application/json'
-		},
-		body: JSON.stringify(message)
-	})
-	.then(response => response.json())
-	.then(data => {
-		console.log('Success:', data);
-	})
-	.catch(error => {
-		console.error('Error:', error);
-	});
-});
+	
+}
