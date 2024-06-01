70271ebe92b44138085c6c01b8e38e4ab64a85b8
Scott Bader <58278360+ManWithACap@users.noreply.github.com>
2024-06-01 05:24:40 +0000

    redirecting on page load?

--- pages/scripts/dashboard.js
+++ pages/scripts/dashboard.js
@@ -1,4 +1,7 @@
+if (!sessionStorage.getItem("username")) {
+	window.location.href = "../../";
+}
+
 window.onload = function() {
 	document.getElementById("username").innerHTML = window.sessionStorage.getItem("username");
 }
-
